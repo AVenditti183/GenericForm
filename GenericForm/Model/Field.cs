13b138ede9127d3053701d5083b9551ee5b574a2
Antonio Venditti <vasoft83@gmail.com>
2020-07-10 23:51:53 +0200

    Aggiungere i file di progetto.

---
+++ GenericForm/Model/Field.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericForm.Model
+{
+    public class Field
+    {
+        public Field(string label,string nameProperty, TypeField typeField, string cssClass)
+        {
+            Label = label;
+            NameProperty = nameProperty;
+            TypeField = typeField;
+            this.cssClass = cssClass;
+        }
+        public string Label { get; set; }
+        public string NameProperty { get; set; }
+        public TypeField TypeField{ get; set; }
+        public string cssClass { get; set; }
+        
+    }
+
+    public enum TypeField
+    {
+        text,
+        number,
+        date 
+    }
+}
---
+++ GenericForm/Model/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericForm.Model
+{
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public int Height { get; set; }
+        
+    }
+}
